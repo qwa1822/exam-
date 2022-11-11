64c7d619b05c95c38b79132c507404a53b9c373b
qwa1822 <asdzxc1822@naver.com>
2022-11-11 22:32:02 +0900

    '객체'

---
+++ Ele/7.object.js
@@ -0,0 +1,25 @@
+let name='apple';
+let color='red';
+let display='💖';
+let orangeName='orange';
+
+let apple={
+  name:'apple',
+  color:'red',
+  display:'❤',
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+let orange={
+  name:'오렌지',
+  color:'yellow',
+};
+
+
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
